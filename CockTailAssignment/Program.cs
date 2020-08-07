48b8f5649babac6aa4b94feb5f50958200724ffb
ShizukuKiri <kira.kira@live.dk>
2020-08-08 01:25:51 +0200

    initial commit

---
+++ CockTailAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CockTailAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
